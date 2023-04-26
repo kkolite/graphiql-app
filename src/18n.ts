4f35bfef55c4d2d2dbb37d0894db906a463dcdf2
Ludmila <milasasha@gmail.com>
2023-04-26 19:02:53 +0300

    refactor: change style code

--- src/18n.ts
+++ src/18n.ts
@@ -3,7 +3,11 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
+i18n
+.use(Backend)
+.use(LanguageDetector)
+.use(initReactI18next)
+.init({
 	fallbackLng: 'ru',
 	returnNull: false,
 	debug: true,
