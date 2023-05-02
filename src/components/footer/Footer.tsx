7d87e2985270a4a301f2667035c9c7ec18163123
Yahor Litavar <sergioegor21@gmail.com>
2023-05-02 19:51:23 +0300

    refactor

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,9 +2,7 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <div className="footer__items">
-          footer
-        </div>
+        <div className="footer__items">footer</div>
       </div>
     </footer>
   );
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,16 +1,22 @@
-import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth, signInWithCustomToken, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithCustomToken,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 
-import { getToken, removeToken, setToken } from "./cookie";
+import { getToken, removeToken, setToken } from './cookie';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbPIH_e5UAfYHCr3L7tIdmgRqU5tWfFhc",
-  authDomain: "graphiql-auth.firebaseapp.com",
-  projectId: "graphiql-auth",
-  storageBucket: "graphiql-auth.appspot.com",
-  messagingSenderId: "321298717669",
-  appId: "1:321298717669:web:fdc941433daec2fc926d0c"
+  apiKey: 'AIzaSyAbPIH_e5UAfYHCr3L7tIdmgRqU5tWfFhc',
+  authDomain: 'graphiql-auth.firebaseapp.com',
+  projectId: 'graphiql-auth',
+  storageBucket: 'graphiql-auth.appspot.com',
+  messagingSenderId: '321298717669',
+  appId: '1:321298717669:web:fdc941433daec2fc926d0c',
 };
 
 const app = initializeApp(firebaseConfig);
@@ -32,15 +38,15 @@ export const fbRegister = async (name: string, email: string, password: string) 
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
     const user = res.user;
-    await addDoc(collection(db, "users"), {
+    await addDoc(collection(db, 'users'), {
       uid: user.uid,
       name,
-      authProvider: "local",
+      authProvider: 'local',
       email,
     });
     return true;
   } catch {
-    return false
+    return false;
   }
 };
 
@@ -58,4 +64,4 @@ export const fbCheck = async () => {
   } catch {
     return false;
   }
-}
+};
