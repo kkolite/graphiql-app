d6c9f481cd1b7b60f04735d892a00e889d3e33c7
Konstantin Moskvin <mr.koctia@gmail.com>
2023-05-03 15:51:16 +0300

    feat: add redux modules and add space store

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,15 +2,32 @@ import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { EPages } from '../../data/types';
 
+import { setBtnSignIn, setBtnSignUp } from '../../store/slice/headerSlice';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+
 import logo from '../../assets/graphql.svg';
 import '../../styles/header.scss';
 
 export const Header = () => {
+  const dispatch = useAppDispatch();
+  const { isSignIn, isSignUp } = useAppSelector((state) => state.headers);
   const { t, i18n } = useTranslation();
 
   const changeLang = (lang: string) => {
     i18n.changeLanguage(lang);
   };
+
+  const changeSign = (status: string) => {
+    if (status === 'in') {
+      dispatch(setBtnSignIn(true));
+      dispatch(setBtnSignUp(false));
+    }
+    if (status === 'up') {
+      dispatch(setBtnSignIn(false));
+      dispatch(setBtnSignUp(true));
+    }
+  };
+
   return (
     <header className="header">
       <div className="header__container">
@@ -19,6 +36,7 @@ export const Header = () => {
             <img src={logo} alt="" />
           </a>
         </div>
+
         <nav className="header__menu nav">
           <NavLink className="nav__item" to={EPages.HOME}>
             {t('pageHome')}
@@ -30,10 +48,17 @@ export const Header = () => {
             {t('pageLogin')}
           </NavLink>
         </nav>
+
         <div className="header__btn-box">
+          {isSignIn && <div>Sign in</div>}
+          {isSignUp && <div>Sign up</div>}
           <div className="header__sign">
-            <button>Sign in</button>
-            <button>Sign up</button>
+            <button className={isSignIn ? 'header__active' : ''} onClick={() => changeSign('in')}>
+              Sign in
+            </button>
+            <button className={isSignUp ? 'header__active' : ''} onClick={() => changeSign('up')}>
+              Sign up
+            </button>
           </div>
           <div className="header__lang">
             <button onClick={() => changeLang('en')}>EN</button>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { Provider } from 'react-redux';
+import { store } from './store';
+
 import App from './App.tsx';
 import './18n.ts';
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
