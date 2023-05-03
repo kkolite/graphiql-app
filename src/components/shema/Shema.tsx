4de668ea0d05e5d87c2ca7c603bb4379c6016dd4
Ludmila <milasasha@gmail.com>
2023-05-03 13:04:53 +0300

    feat: add query on input

--- src/components/shema/Shema.tsx
+++ src/components/shema/Shema.tsx
@@ -22,52 +22,52 @@ export const postsSubschema = {
   executor: remoteExecutor,
 };
 
-const rootTypes = [
-  postsSubschema.schema.getQueryType(),
-  postsSubschema.schema.getMutationType(),
-  postsSubschema.schema.getSubscriptionType(),
-].filter((x) => !!x);
-
-console.log(rootTypes);
-const name: string[] = [];
-rootTypes.forEach((type: Maybe<GraphQLObjectType<string, GraphQLType>>) => {
-  if (type) {
-    console.log('isTypeOf ', type.isTypeOf);
-    console.log('toJSON ', type.toJSON());
-    console.log('toConfig ', type.toConfig());
-    console.log('getFields', type.getFields());
-    console.log('getType', postsSubschema.schema.getType('Continent'));
-    const fields = type.getFields();
-    Object.values(fields).forEach((fieldName) => {
-      console.log('fieldName', fieldName);
-      name.push(fieldName.name);
-      // console.log('name', fieldName.name);
-      console.log('type', fieldName.type);
-      // console.log('type', fieldName.type);
-      // if (fieldName.type == 'GraphQLObjectType')
-
-      // console.log('index', index);
-      // console.log('fields[fieldName]', fields[fieldName]);
-      // console.log(fields[fieldName].astNode);
-      // if (!fields[fieldName].astNode) {
-      //   const astNode = graphQLCustomTypeDef.fields.find(field => field.name.value === graphQLSchemaTypeFieldName);
-
-      //   if (astNode) {
-      //    graphQLSchemaTypeField.astNode = astNode;
-      //   }
-      //  }
-    });
-  }
-});
-
 interface IPropsModel {
-  // props: link;
-  onClose?: () => void | undefined;
+  // onClose?: () => void | undefined;
+  endpoint: string;
 }
 
-export const Shema = ({ onClose }: IPropsModel) => {
+export const Shema = ({ endpoint }: IPropsModel) => {
+  console.log('endpoint', endpoint);
+  const rootTypes = [
+    postsSubschema.schema.getQueryType(),
+    postsSubschema.schema.getMutationType(),
+    postsSubschema.schema.getSubscriptionType(),
+  ].filter((x) => !!x);
+
+  console.log(rootTypes);
+  const name: string[] = [];
+  rootTypes.forEach((type: Maybe<GraphQLObjectType<string, GraphQLType>>) => {
+    if (type) {
+      console.log('isTypeOf ', type.isTypeOf);
+      console.log('toJSON ', type.toJSON());
+      console.log('toConfig ', type.toConfig());
+      console.log('getFields', type.getFields());
+      console.log('getType', postsSubschema.schema.getType('Continent'));
+      const fields = type.getFields();
+      Object.values(fields).forEach((fieldName) => {
+        console.log('fieldName', fieldName);
+        name.push(fieldName.name);
+        // console.log('name', fieldName.name);
+        console.log('type', fieldName.type);
+        // console.log('type', fieldName.type);
+        // if (fieldName.type == 'GraphQLObjectType')
+
+        // console.log('index', index);
+        // console.log('fields[fieldName]', fields[fieldName]);
+        // console.log(fields[fieldName].astNode);
+        // if (!fields[fieldName].astNode) {
+        //   const astNode = graphQLCustomTypeDef.fields.find(field => field.name.value === graphQLSchemaTypeFieldName);
+
+        //   if (astNode) {
+        //    graphQLSchemaTypeField.astNode = astNode;
+        //   }
+        //  }
+      });
+    }
+  });
   return (
-    <div id={`1`} className="modal" onClick={onClose}>
+    <div id={`1`} className="modal">
       <h2>Documentation Explorer</h2>
       <pre>{printSchema(postsSubschema.schema)}</pre>
     </div>
