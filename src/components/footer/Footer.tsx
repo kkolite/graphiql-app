a672507f3e80b0ce256765429a557e868f9d3368
Konstantin Moskvin <mr.koctia@gmail.com>
2023-05-05 16:35:14 +0300

    feat: add style and element in footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,35 @@
+import './footer.scss';
+import rsschool from '../../assets/rs_shool.svg';
+import github from '../../assets/github.png';
+
 export const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__container">
-        <div className="footer__items">footer</div>
+      <div className="footer__box footer__container">
+        <div className="footer__github">
+          <div className="footer__project">
+            <a href="https://github.com/kkolite/graphiql-app" target="_blank">
+              <img src={github} alt="" />
+            </a>
+          </div>
+          <div className="footer__link">
+            <a href="https://github.com/kkolite" target="_blank">
+              kkolite
+            </a>
+            <a href="https://github.com/koctia" target="_blank">
+              koctia
+            </a>
+            <a href="https://github.com/motoblock" target="_blank">
+              motoblock
+            </a>
+          </div>
+        </div>
+        <div className="footer__copywrite">RS-GraphQL © 2023 All rights reserved.</div>
+        <div className="footer__rsschool">
+          <a href="https://rs.school/react/" target="_blank">
+            <img src={rsschool} alt="" />
+          </a>
+        </div>
       </div>
     </footer>
   );
