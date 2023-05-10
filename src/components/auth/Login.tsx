860414d7b9c92da9eff92ca86f5159536cc29351
Konstantin Moskvin <mr.koctia@gmail.com>
2023-05-10 15:30:25 +0300

    feat: add modal windows in login and register forms

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useAppDispatch } from '../../store/hooks';
 
 import { fbLogin } from '../../utils/firebase';
 import { validate } from '../../utils/validate';
 import { EValidate } from '../../data/types';
+import { setBtnSignIn } from '../../store/slice/headerSlice';
+import './modal.scss';
 
 export const Login = () => {
+  const dispatch = useAppDispatch();
+
   const [email, setEmail] = useState('');
   const [emailError, setEmailError] = useState('');
   const [password, setPassword] = useState('');
@@ -32,21 +37,33 @@ export const Login = () => {
     console.log(result);
   };
 
+  const handleClick = () => {
+    dispatch(setBtnSignIn(false));
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <input type="email" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} />
-        <label>{emailError}</label>
+    <div className="modal" onClick={handleClick}>
+      <div className="modal__box" onClick={(e) => e.stopPropagation()}>
+        <div className="modal__close">
+          <div onClick={handleClick}>X</div>
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input type="email" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} />
+            <label>{emailError}</label>
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder={t('password') as string}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label>{passwordError}</label>
+          </div>
+          <button>{t('logIn')}</button>
+        </form>
       </div>
-      <div>
-        <input
-          type="password"
-          placeholder={t('password') as string}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <label>{passwordError}</label>
-      </div>
-      <button>{t('logIn')}</button>
-    </form>
+    </div>
   );
 };
