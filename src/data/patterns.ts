2b35bda9a8e4b911685bb713b077c5e0dc146194
Yahor Litavar <sergioegor21@gmail.com>
2023-05-16 12:00:30 +0300

    fix: const in cookie

--- src/data/patterns.ts
+++ src/data/patterns.ts
@@ -2,5 +2,8 @@ export const PATTERN = {
   PASSWORD: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/g,
   USERNAME: /^[A-Za-z0-9_]{2,13}$/g,
   /* eslint-disable */
-  EMAIL: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+  EMAIL: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+  TOKEN: /([\.$?*|{}\(\)\[\]\\\/\+^])/g,
+  TOKEN_START: '(?:^|; )',
+  TOKEN_END: '=([^;]*)',
 };
